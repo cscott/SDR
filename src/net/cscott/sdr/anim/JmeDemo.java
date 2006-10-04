4e0e026aa1f5d59175296a4e084f6b6177e7b3de
C. Scott Ananian <cscott@cscott.net>
2006-10-04 22:36:11 +0000

    Complete checkers scene, with colors, arrows, and labels.

--- src/net/cscott/sdr/anim/JmeDemo.java
+++ src/net/cscott/sdr/anim/JmeDemo.java
@@ -5,10 +5,16 @@ import com.jme.bounding.BoundingBox;
 import com.jme.image.Texture;
 import com.jme.math.Quaternion;
 import com.jme.math.Vector3f;
+import com.jme.renderer.ColorRGBA;
+import com.jme.scene.Node;
+import com.jme.scene.SharedNode;
+import com.jme.scene.Spatial;
 import com.jme.scene.shape.Box;
 import com.jme.scene.shape.Cylinder;
 import com.jme.scene.shape.Quad;
 import com.jme.scene.shape.Sphere;
+import com.jme.scene.state.AlphaState;
+import com.jme.scene.state.MaterialState;
 import com.jme.scene.state.TextureState;
 import com.jme.util.LoggingSystem;
 import com.jme.util.TextureManager;
@@ -17,10 +23,7 @@ import java.net.URL;
 
 public class JmeDemo extends SimpleGame {
 
-  private Quaternion rotQuat = new Quaternion();
-  private float angle = 0;
-  private Vector3f axis = new Vector3f(1, 1, 0);
-  private Sphere s;
+  private Node[] checker = new Node[8];
 
   /**
    * Entry point for the test,
@@ -55,31 +58,16 @@ public class JmeDemo extends SimpleGame {
   protected void simpleInitGame() {
     display.setTitle("SDR - jME demo");
     cam.setLocation(new Vector3f(0, -8.5f, 9.4f));
+    cam.lookAt(new Vector3f(0,0,0), new Vector3f(0,0,1));
 
-
-    s = new Sphere("Sphere", 63, 50, 25);
-    s.setLocalTranslation(new Vector3f(0,0,-40));
-    s.setModelBound(new BoundingBox());
-    s.updateModelBound();
-    rootNode.attachChild(s);
-
-    TextureState ts = display.getRenderer().createTextureState();
-    ts.setEnabled(true);
-    ts.setTexture(
-        TextureManager.loadTexture(
-        JmeDemo.class.getClassLoader().getResource(
-        "net/cscott/sdr/splash.png"),
-        Texture.MM_LINEAR_LINEAR,
-        Texture.FM_LINEAR));
-    s.setRenderState(ts);
-
+    // create floor.
     Quad q = new Quad("floor", 10, 10);
     q.setLocalTranslation(q.getCenter().negate());
     q.setModelBound(new BoundingBox());
     q.updateModelBound();
     rootNode.attachChild(q);
 
-    ts = display.getRenderer().createTextureState();
+    TextureState ts = display.getRenderer().createTextureState();
     ts.setEnabled(true);
     ts.setTexture
 	(TextureManager.loadTexture
@@ -88,40 +76,95 @@ public class JmeDemo extends SimpleGame {
 	  Texture.MM_LINEAR_LINEAR,
 	  Texture.FM_LINEAR));
     q.setRenderState(ts);
+    MaterialState ms = display.getRenderer().createMaterialState();
+    ms.setAmbient(new ColorRGBA(1,1,1,1));
+    q.setRenderState(ms);
 
-    Cylinder c = new Cylinder("1girl", 16, 16, 0.7f, 0.26f, true);
-    c.setLocalTranslation(new Vector3f(1f,-3f,.13f));
-    c.setModelBound(new BoundingBox());
-    c.updateModelBound();
-    rootNode.attachChild(c);
-
-    ts = display.getRenderer().createTextureState();
-    ts.setEnabled(true);
-    ts.setTexture
-	(TextureManager.loadTexture
-	 (JmeDemo.class.getClassLoader().getResource
-	  ("net/cscott/sdr/1girl.png"),
-	  Texture.MM_LINEAR_LINEAR,
-	  Texture.FM_LINEAR));
-    c.setRenderState(ts);
-
-    Box b = new Box("1boy",
-		    new Vector3f(-0.7f, -0.7f, 0),
-		    new Vector3f(0.7f, 0.7f, 0.26f));
-    b.setLocalTranslation(new Vector3f(-1f,-3f,0));
-    b.setModelBound(new BoundingBox());
-    b.updateModelBound();
-    rootNode.attachChild(b);
-
-    ts = display.getRenderer().createTextureState();
-    ts.setEnabled(true);
-    ts.setTexture
-	(TextureManager.loadTexture
-	 (JmeDemo.class.getClassLoader().getResource
-	  ("net/cscott/sdr/1boy.png"),
-	  Texture.MM_LINEAR_LINEAR,
-	  Texture.FM_LINEAR));
-    b.setRenderState(ts);
-
+    // create materials for the couples
+    MaterialState[] mats = new MaterialState[4];
+    ColorRGBA[] colors = new ColorRGBA[] {
+	new ColorRGBA(1,10/255f,18/255f,1),
+	new ColorRGBA(0,135/255f,250/255f,1),
+	new ColorRGBA(240/255f,1,69/255f,1),
+	new ColorRGBA(151/255f,1,84/255f,1),
+    };
+    for (int i=0; i<mats.length; i++) {
+	mats[i] = display.getRenderer().createMaterialState();
+	mats[i].setAmbient(colors[i]);
+    }
+    // create nodes for the couples.
+    int[] chx = new int[] { -1, 1, 3, 3, 1, -1, -3, -3 };
+    int[] chy = new int[] { -3, -3, -1, 1, 3, 3, 1, -1 };
+    for (int i=0; i<8; i++) {
+      checker[i] = new Node("checker"+i);
+      checker[i].setLocalTranslation(new Vector3f(chx[i],chy[i],0));
+      checker[i].setLocalRotation(new Quaternion(new float[] {0,0,(float)((i/2)*Math.PI/2) }));
+      checker[i].updateModelBound();
+      checker[i].setTextureCombineMode(TextureState.COMBINE_FIRST);
+      rootNode.attachChild(checker[i]);
+    }
+      
+    // now create boys.
+    for (int i=0; i<checker.length/2; i++) {
+	Box b = new Box("boy"+i,
+			new Vector3f(-0.7f, -0.7f, 0f),
+			new Vector3f(0.7f, 0.7f, 0.26f));
+	b.setLocalTranslation(new Vector3f(0,0,.01f));
+	b.setModelBound(new BoundingBox());
+	b.updateModelBound();
+	b.setRenderState(mats[i]);
+	checker[i*2].attachChild(b);
+    }
+    // and girls
+    for (int i=0; i<checker.length/2; i++) {
+	Cylinder c = new Cylinder((i+1)+"girl", 16, 16, 0.7f, 0.26f, true);
+	c.setLocalTranslation(new Vector3f(0,0,0.14f));
+	c.setModelBound(new BoundingBox());
+	c.updateModelBound();
+	c.setRenderState(mats[i]);
+	checker[i*2+1].attachChild(c);
+    }
+    // and label them.
+    TextureState arrowTS = display.getRenderer().createTextureState();
+    arrowTS.setEnabled(true);
+    arrowTS.setTexture(TextureManager.loadTexture
+		       (JmeDemo.class.getClassLoader().getResource
+			("net/cscott/sdr/arrow.png"),
+			Texture.MM_LINEAR_LINEAR,
+			Texture.FM_LINEAR));
+    TextureState[] numTS = new TextureState[4];
+    for (int i=0; i<numTS.length; i++) {
+      numTS[i] = display.getRenderer().createTextureState();
+      numTS[i].setEnabled(true);
+      numTS[i].setTexture(TextureManager.loadTexture
+			  (JmeDemo.class.getClassLoader().getResource
+			   ("net/cscott/sdr/"+(i+1)+".png"),
+			   Texture.MM_LINEAR_LINEAR,
+			   Texture.FM_LINEAR));
+    }
+    AlphaState as = display.getRenderer().createAlphaState();
+    as.setBlendEnabled(true);
+    as.setSrcFunction(AlphaState.SB_SRC_ALPHA);
+    as.setDstFunction(AlphaState.SB_ONE_MINUS_SRC_ALPHA);
+    as.setTestEnabled(true);
+    as.setTestFunction(AlphaState.TF_GREATER);
+    
+    for (int i=0; i<8; i++) {
+      boolean isBoy = (0==(i%2));
+      // arrow
+      Quad qq = new Quad("arrow"+i, 1.3f, 1.3f);
+      qq.setLocalTranslation(new Vector3f(0,isBoy?0f:.1f,.3f));
+      qq.setRenderState(arrowTS);
+      qq.setRenderState(as);
+      qq.updateModelBound();
+      checker[i].attachChild(qq);
+      // number
+      qq = new Quad("num"+i, .4f, .4f);
+      qq.setLocalTranslation(new Vector3f(0,0,.32f));
+      qq.setRenderState(numTS[i/2]);
+      qq.setRenderState(as);
+      qq.updateModelBound();
+      checker[i].attachChild(qq);
+    }
   }
 }
