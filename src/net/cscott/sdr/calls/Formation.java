93000cfd3a1bf7aa877c61956d9687525dc934bb
C. Scott Ananian <cscott@cscott.net>
2006-10-08 20:22:56 +0000

    Formations also have dancer tags, such as BELLE, BEAU, LEADER, TRAILER, etc.

--- src/net/cscott/sdr/calls/Formation.java
+++ src/net/cscott/sdr/calls/Formation.java
@@ -3,6 +3,7 @@ package net.cscott.sdr.calls;
 import static net.cscott.sdr.calls.StandardDancer.*;
 
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -12,11 +13,19 @@ import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
+import net.cscott.jutil.*;
 
 /** A Formation is a set of dancers and positions for them.
- * Certain dancers in the formation can be selected. */
+ * Certain dancers in the formation can be selected. 
+ * In addition, all dancers can be tagged with modifiers
+ * such as 'BEAU', 'BELLE', 'LEADER', 'TRAILER', etc.
+ * */
 public class Formation {
+    public enum Identifier {
+        BEAU, BELLE, LEADER, TRAILER;
+    };
     private final Map<Dancer,Position> location;
+    private final Map<Dancer,Set<Identifier>> tags;
     private final Set<Dancer> selected;
 
     public boolean isSelected(Dancer d) {
@@ -51,43 +60,66 @@ public class Formation {
 	    .append("selected", selected)
 	    .toString();
     }
+    // starting formation for 8-couple dancing
     public static final Formation SQUARED_SET = new Formation
 	(new DancerInfo(COUPLE_1_BOY,
-			Position.getGrid(-1,-3,Rotation.ZERO)),
+			Position.getGrid(-1,-3,Rotation.ZERO), Identifier.BEAU),
 	 new DancerInfo(COUPLE_1_GIRL,
-			Position.getGrid(+1,-3,Rotation.ZERO)),
+			Position.getGrid(+1,-3,Rotation.ZERO), Identifier.BELLE),
 	 new DancerInfo(COUPLE_2_BOY,
-			Position.getGrid(+3,-1,Rotation.THREE_QUARTERS)),
+			Position.getGrid(+3,-1,Rotation.THREE_QUARTERS), Identifier.BEAU),
 	 new DancerInfo(COUPLE_2_GIRL,
-			Position.getGrid(+3,+1,Rotation.THREE_QUARTERS)),
+			Position.getGrid(+3,+1,Rotation.THREE_QUARTERS), Identifier.BELLE),
 	 new DancerInfo(COUPLE_3_BOY,
-			Position.getGrid(+1,+3,Rotation.ONE_HALF)),
+			Position.getGrid(+1,+3,Rotation.ONE_HALF), Identifier.BEAU),
 	 new DancerInfo(COUPLE_3_GIRL,
-			Position.getGrid(-1,+3,Rotation.ONE_HALF)),
+			Position.getGrid(-1,+3,Rotation.ONE_HALF), Identifier.BELLE),
 	 new DancerInfo(COUPLE_4_BOY,
-			Position.getGrid(-3,+1,Rotation.ONE_QUARTER)),
+			Position.getGrid(-3,+1,Rotation.ONE_QUARTER), Identifier.BEAU),
 	 new DancerInfo(COUPLE_4_GIRL,
-			Position.getGrid(-3,-1,Rotation.ONE_QUARTER))
+			Position.getGrid(-3,-1,Rotation.ONE_QUARTER), Identifier.BELLE)
 	 );
+    // starting formation for 2-couple dancing.
+    public static final Formation FOUR_SQUARE = new Formation
+        (new DancerInfo(COUPLE_1_BOY,
+                Position.getGrid(-1,-1,Rotation.ZERO), Identifier.BEAU),
+         new DancerInfo(COUPLE_1_GIRL,
+                Position.getGrid(+1,-1,Rotation.ZERO), Identifier.BELLE),
+         new DancerInfo(COUPLE_3_BOY,
+                Position.getGrid(+1,+1,Rotation.ONE_HALF), Identifier.BEAU),
+         new DancerInfo(COUPLE_3_GIRL,
+                Position.getGrid(-1,+1,Rotation.ONE_HALF), Identifier.BELLE)
+        );
 
     Formation(DancerInfo... dis) {
 	Map<Dancer,Position> m = new HashMap<Dancer,Position>();
 	Set<Dancer> s = new HashSet<Dancer>();
+        Map<Dancer,Set<Identifier>> t = new HashMap<Dancer,Set<Identifier>>();
 	for (DancerInfo di : dis) {
 	    m.put(di.dancer, di.position);
-	    if (di.isSelected)
+	    t.put(di.dancer, Collections.unmodifiableSet(di.tags));
+            if (di.isSelected)
 		s.add(di.dancer);
 	}
 	this.location = Collections.unmodifiableMap(m);
 	this.selected = Collections.unmodifiableSet(s);
+        this.tags = Collections.unmodifiableMap(t);
     }
     static class DancerInfo {
 	final Dancer dancer;
 	final Position position;
-	final boolean isSelected;
-	DancerInfo(Dancer d, Position p, boolean s) {
-	    this.dancer = d; this.position = p; this.isSelected = s;
+	final Set<Identifier> tags;
+        final boolean isSelected;
+	DancerInfo(Dancer d, Position p, Set<Identifier> tags, boolean s) {
+	    this.dancer = d; this.position = p;
+	    this.tags = tags; this.isSelected = s;
 	}
-	DancerInfo(Dancer d, Position p) { this(d,p,true); }
+	DancerInfo(Dancer d, Position p, Identifier... tags) {
+            this(d,p,setOf(tags),true);
+        }
+    }
+    private static Set<Identifier> setOf(Identifier... tags) {
+        if (tags.length==0) return EnumSet.noneOf(Identifier.class);
+        else return EnumSet.of(tags[0], tags);
     }
 }
