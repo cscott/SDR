84bbf29d6277b80bb90f40ebdddd34fc1d94d210
C. Scott Ananian <cscott@cscott.net>
2006-10-10 20:39:38 +0000

    Add Warp.compose(); add javadoc.

--- src/net/cscott/sdr/calls/Warp.java
+++ src/net/cscott/sdr/calls/Warp.java
@@ -10,7 +10,7 @@ import net.cscott.sdr.util.Fraction;
  * box formation starts with boxes overlapping at time t=0, and
  * results in an undistorted setup at time t=1.
  * @author C. Scott Ananian
- * @version $Id: Warp.java,v 1.2 2006-10-10 04:32:42 cananian Exp $
+ * @version $Id: Warp.java,v 1.3 2006-10-10 20:39:38 cananian Exp $
  */
 public abstract class Warp {
     // XXX: not sure how best to implement this generally yet.
@@ -25,16 +25,21 @@ public abstract class Warp {
 
     public abstract Position warp(Position p, Fraction time);
 
+    /** A <code>Warp</code> which returns points unchanged. */
     public static final Warp NONE = new Warp() {
         public Position warp(Position p, Fraction time) { return p; }
     };
     
+    /** A <code>Warp</code> object that mirrors points across the y-axis. */
     public static final Warp MIRROR = new Warp() {
         public Position warp(Position p, Fraction time) {
             return new Position(p.x.negate(), p.y, p.facing.negate());
         }
     };
     
+    /** Returns a <code>Warp</code> which will rotate and translate
+     * points such that <code>from</code> is warped to <code>to</code>.
+     */
     public static Warp rotateAndMove(Position from, Position to) {
         Rotation rot = to.facing.add(from.facing.amount.negate());
         Position nFrom = rotateCWAroundOrigin(from,rot);
@@ -57,4 +62,15 @@ public abstract class Warp {
         return new Position(x, y, p.facing.add(amt.amount));
     }
     
+    /** Returns a new <code>Warp</code> object which first applies the
+     * <code>first</code> warp to the given point, then applies the
+     * <code>second</code> warp to the result.
+     */
+    public static Warp compose(final Warp second, final Warp first) {
+        return new Warp() {
+            public Position warp(Position p, Fraction time) {
+                return second.warp(first.warp(p,time), time);
+            }
+        };
+    }
 }
