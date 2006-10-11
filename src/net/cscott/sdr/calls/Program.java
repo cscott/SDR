75b32d23465d6b9d8b42d6da786c58544db5011c
C. Scott Ananian <cscott@cscott.net>
2006-10-11 15:02:33 +0000

    Implement a method to give Program ordering.

--- src/net/cscott/sdr/calls/Program.java
+++ src/net/cscott/sdr/calls/Program.java
@@ -4,4 +4,15 @@ public enum Program {
     // There are probably other programs that could be used here:
     // Vic Cedar's "variant" lists, the different C4/C4X lists, etc.
     BASIC, MAINSTREAM, PLUS, A1, A2, C1, C2, C3A, C3B, C4;
+    /**
+     * Implement an ordering on programs.  Right now this is the same as
+     * the ordinal numbering for the enumeration, but when we implement
+     * 'variants', then the variant lists should split off (so that C1V
+     * includes C1 and A2V, but C1 doesn't include A2V).   Best way to
+     * implement this (again, eventually) is to have each class indicate
+     * what the class directly below it is, and recursively define inclusion.
+     */
+    public boolean includes(Program p) {
+        return this.ordinal() >= p.ordinal();
+    }
 }
