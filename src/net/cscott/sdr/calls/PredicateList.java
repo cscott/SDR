ec4ae490049f6eaf95659cc20f76775b8eba2d25
C. Scott Ananian <cscott@cscott.net>
2006-10-11 19:15:25 +0000

    Add 'and' and 'or' short-circuit operators.

--- src/net/cscott/sdr/calls/PredicateList.java
+++ src/net/cscott/sdr/calls/PredicateList.java
@@ -4,16 +4,8 @@ import net.cscott.sdr.calls.ast.Condition;
 
 /** This class contains all the predicates known to the system. */
 public abstract class PredicateList {
-
     
-    public final static Predicate NOT = new _Predicate("not") {
-        @Override
-        public boolean evaluate(DanceState ds, Formation f, Condition c) {
-            assert c.getNumberOfChildren()==1;
-            Condition arg = c.getArg(0);
-            return !arg.getPredicate().evaluate(ds,f,arg);
-        }
-    };
+    // zero-arg operators
     public final static Predicate TRUE = new _Predicate("true") {
         @Override
         public boolean evaluate(DanceState ds, Formation f, Condition c) {
@@ -28,6 +20,43 @@ public abstract class PredicateList {
             return false;
         }
     };
+    // one-arg operators
+    public final static Predicate NOT = new _Predicate("not") {
+        @Override
+        public boolean evaluate(DanceState ds, Formation f, Condition c) {
+            assert c.getNumberOfChildren()==1;
+            Condition arg = c.getArg(0);
+            return !arg.getPredicate().evaluate(ds,f,arg);
+        }
+    };
+    // n-ary operators.
+    public final static Predicate AND = new _Predicate("and") {
+        @Override
+        public boolean evaluate(DanceState ds, Formation f, Condition c) {
+            assert c.getNumberOfChildren()>0;
+            boolean result = true;
+            for (int i=0; i<c.getNumberOfChildren(); i++) {
+                Condition cc = c.getArg(i);
+                result = cc.getPredicate().evaluate(ds, f, cc);
+                if (!result) break; // short-circuit operator.
+            }
+            return result;
+        }
+    };
+    public final static Predicate OR = new _Predicate("or") {
+        @Override
+        public boolean evaluate(DanceState ds, Formation f, Condition c) {
+            assert c.getNumberOfChildren()>0;
+            boolean result = false;
+            for (int i=0; i<c.getNumberOfChildren(); i++) {
+                Condition cc = c.getArg(i);
+                result = cc.getPredicate().evaluate(ds, f, cc);
+                if (result) break; // short-circuit operator.
+            }
+            return result;
+        }
+    };
+    // okay, square-dance-specific operators.
     public final static Predicate PROGRAM_AT_LEAST = new _Predicate("program at least") {
         @Override
         public boolean evaluate(DanceState ds, Formation f, Condition c) {
