db7b0fb87d57e46355f9aeddb46abaebfe987d54
C. Scott Ananian <cscott@cscott.net>
2006-10-12 13:28:43 +0000

    Add BadCallException.

---
+++ src/net/cscott/sdr/calls/BadCallException.java
@@ -0,0 +1,7 @@
+package net.cscott.sdr.calls;
+
+/** This exception indicates that a call was invalid. */
+public class BadCallException extends RuntimeException {
+    public BadCallException() { super(); }
+    public BadCallException(String s) { super(s); }
+}
