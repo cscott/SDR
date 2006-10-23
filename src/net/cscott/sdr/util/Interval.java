30e772fdbb079c9a5429d51fdcfcf58b8200bb4a
C. Scott Ananian <cscott@cscott.net>
2006-10-23 16:54:12 +0000

    Improve javadoc.

--- src/net/cscott/sdr/util/Interval.java
+++ src/net/cscott/sdr/util/Interval.java
@@ -9,14 +9,20 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
  * The start fraction must always be less than or equal to the end
  * fraction.
  * @author C. Scott Ananian
- * @version $Id: Interval.java,v 1.1 2006-10-23 16:51:09 cananian Exp $
+ * @version $Id: Interval.java,v 1.2 2006-10-23 16:54:12 cananian Exp $
  */
 public class Interval implements Comparable<Interval> {
     public final Fraction start, end;
+    /** Create an {@link Interval} from {@code start} to {@code end}.
+     * The {@code start} parameter must be less than or equal to {@code end}.
+     */
     public Interval(Fraction start, Fraction end) {
         this.start=start; this.end=end;
         assert this.start.compareTo(this.end) <= 0;
     }
+    /** Compare two {@link Interval}s.  Intervals are ordered first by
+     * start, and if the starts are equal, by their ends.
+     */
     public int compareTo(Interval i) {
         int c = this.start.compareTo(i.start);
         if (c!=0) return c;
