aba55555d76b947f712bef9d38a5917a952c130c
C. Scott Ananian <cscott@cscott.net>
2006-10-25 22:55:18 +0000

    Create the CommandInput class, which synchronizes the various input methods with the choreography engine.

--- src/net/cscott/sdr/App.java
+++ src/net/cscott/sdr/App.java
@@ -10,7 +10,7 @@ import net.cscott.sdr.anim.SdrGame;
  * and one to play music (in net.cscott.sdr.sound).
  * 
  * @author C. Scott Ananian
- * @version $Id: App.java,v 1.1 2006-10-06 18:15:00 cananian Exp $
+ * @version $Id: App.java,v 1.2 2006-10-25 22:55:18 cananian Exp $
  */
 public class App {
 	/**
@@ -22,5 +22,13 @@ public class App {
 		// TODO: create music player thread
 		SdrGame game = new SdrGame();
 		game.start();
+                
+                // At all points during the game we have a current
+                // TimedFormation.  We grab strings from the input
+                // and parse them using the CommandInput, and apply them to the
+                // current TimedFormation to get a time-sorted list of
+                // additional TimedFormations.  We have to go through these
+                // to extract TimedPositions for each dancer, which are
+                // given to the AnimDancers.
 	}
 }
