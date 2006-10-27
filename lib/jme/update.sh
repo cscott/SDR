f14c324f47573b1347eaa544cad3721e424df5b9
C. Scott Ananian <cscott@cscott.net>
2006-10-27 15:47:46 +0000

    Update JME to CVS-latest.

--- lib/jme/update.sh
+++ lib/jme/update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-JME_CVS=../../jme-dist/jme
+JME_CVS=../../dist/jme
 cp ${JME_CVS}/target/*.jar ${JME_CVS}/lib/*.{so,jar,dll,jnilib,dylib} \
    ${JME_CVS}/lib/mvn-lib-install .
 /bin/rm lwjgl_test.jar jmetest.jar jmetest-data.jar
