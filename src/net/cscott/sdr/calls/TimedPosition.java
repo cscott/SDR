cf0337eefcc42452673f76983f94f760cf4810a5
C. Scott Ananian <cscott@cscott.net>
2006-10-27 05:14:34 +0000

    TimedPosition now implements Comparable<TimedPosition>.

--- src/net/cscott/sdr/calls/TimedPosition.java
+++ src/net/cscott/sdr/calls/TimedPosition.java
@@ -7,9 +7,9 @@ import net.cscott.sdr.util.Fraction;
  * a timestamp indicating when that particular position
  * should be reached.  The timestamp is always absolute.
  * @author C. Scott Ananian
- * @version $Id: TimedPosition.java,v 1.2 2006-10-26 18:33:19 cananian Exp $
+ * @version $Id: TimedPosition.java,v 1.3 2006-10-27 05:14:34 cananian Exp $
  */
-public class TimedPosition {
+public class TimedPosition implements Comparable<TimedPosition> {
     /** The dancer position called for at the given time. */
     public final Position position;
     /** If {@link TimedPosition#isAbsolute isAbsolute} is true, then the
@@ -24,5 +24,17 @@ public class TimedPosition {
     public TimedPosition(Position position, Fraction time, boolean isAbsolute) {
         this.position = position; this.time = time;
         this.isAbsolute = isAbsolute;
+    }
+    /** {@link TimedPosition}s are compared to each other on the basis
+     * of their {@link TimedPosition#time time} fields.
+     * Earlier {@code TimedPosition}s are before later ones.
+     * @throws IllegalArgumentException if the position times are not
+     *   absolute.
+     */
+    public int compareTo(TimedPosition o) throws IllegalArgumentException {
+        if (this.isAbsolute && ((TimedPosition)o).isAbsolute)
+            return this.time.compareTo(((TimedPosition)o).time);
+        throw new IllegalArgumentException("Comparison is meaningless unless"+
+                " position times are absolute.");
     }
 }
