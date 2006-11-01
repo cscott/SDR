9e8a68ea8f330f5aa2438f710af537f7358388b8
C. Scott Ananian <cscott@cscott.net>
2006-11-01 00:30:11 +0000

    Build a java web start-able version of SDR.
    To this end, separate jar libraries which we need for development but not
    for running the game into lib/dev/.  Also, check in the bundled-up versions
    of the native libraries needed for Java Web Start into lib/jme/jnlp.

--- lib/jme/update.sh
+++ lib/jme/update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+# first build 'webdist-nativelibs' in dist/jme
 JME_CVS=../../dist/jme
 cp ${JME_CVS}/target/*.jar ${JME_CVS}/lib/*.{so,jar,dll,jnilib,dylib} \
    ${JME_CVS}/lib/mvn-lib-install .
 /bin/rm lwjgl_test.jar jmetest.jar jmetest-data.jar
+mkdir -p jnlp
+cp ${JME_CVS}/jnlp/*.jar jnlp
--- upload.sh
+++ upload.sh
@@ -4,19 +4,28 @@
 PACKAGE=sdr
 VERSION=0.1
 # build prerequisites
-ant dist
+ant dist sign-jars
 # make upload bundle
 /bin/rm -rf ${PACKAGE}-${VERSION}
 mkdir ${PACKAGE}-${VERSION}
+# API docs
 tar -c api | tar -C ${PACKAGE}-${VERSION} -x
 mv ${PACKAGE}-${VERSION}/api ${PACKAGE}-${VERSION}/doc
+# version information
+touch ${PACKAGE}-${VERSION}/VERSION_${VERSION}
 (date ; echo "Released $PACKAGE $VERSION" ; echo " " ; cat ChangeLog)\
                 > ${PACKAGE}-${VERSION}/ChangeLog.txt
+# sources & binaries
 cp ${PACKAGE}-${VERSION}.tar.gz ${PACKAGE}-${VERSION}/
 ( cd ${PACKAGE}-${VERSION} && \
     ln -s ${PACKAGE}-${VERSION}.tar.gz ${PACKAGE}.tar.gz )
 cp ${PACKAGE}.jar ${PACKAGE}-${VERSION}/
-touch ${PACKAGE}-${VERSION}/VERSION_${VERSION}
+# Java web start stuff
+cp sdr.jnlp ${PACKAGE}-${VERSION}/
+mkdir -p ${PACKAGE}-${VERSION}/lib
+cp sdr-libs.jar ${PACKAGE}-${VERSION}/lib
+cp lib/jme/jnlp/*.jar ${PACKAGE}-${VERSION}/lib
+# transfer to the distribution machine.
 tar -c ${PACKAGE}-${VERSION}/ | \
     ssh k2.csail.mit.edu "mkdir -p public_html/Projects/SDR && cd public_html/Projects/SDR && /bin/rm -rf ${PACKAGE}-${VERSION} && tar -xv"
 /bin/rm -rf ${PACKAGE}-${VERSION}
