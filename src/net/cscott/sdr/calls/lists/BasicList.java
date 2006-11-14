c1a1c3b64d9f8e3ad0427dcd6791a36a6bb589ee
C. Scott Ananian <cscott@cscott.net>
2006-11-14 16:12:39 +0000

    Add note re: "where are the rest of the calls?"

--- src/net/cscott/sdr/calls/lists/BasicList.java
+++ src/net/cscott/sdr/calls/lists/BasicList.java
@@ -18,8 +18,12 @@ import net.cscott.sdr.util.Fraction;
 /** 
  * The <code>BasicList</code> class contains complex call
  * and concept definitions which are on the 'basic' program.
+ * Note that "simple" calls and concepts are defined in
+ * the resource file at <code>net/cscott/sdr/calls/lists/basic.calls</code>;
+ * this class contains only those definitions for which an
+ * executable component is required.
  * @author C. Scott Ananian
- * @version $Id: BasicList.java,v 1.17 2006-10-27 21:29:51 cananian Exp $
+ * @version $Id: BasicList.java,v 1.18 2006-11-14 16:12:39 cananian Exp $
  */
 public abstract class BasicList {
     // hide constructor.
--- src/net/cscott/sdr/calls/lists/MainstreamList.java
+++ src/net/cscott/sdr/calls/lists/MainstreamList.java
@@ -6,8 +6,13 @@ import net.cscott.sdr.calls.Program;
 /** 
  * The <code>MainstreamList</code> class contains complex call
  * and concept definitions which are on the 'mainstream' program.
+ * Note that "simple" calls and concepts are defined in
+ * the resource file at
+ * <code>net/cscott/sdr/calls/lists/mainstream.calls</code>;
+ * this class contains only those definitions for which an
+ * executable component is required.
  * @author C. Scott Ananian
- * @version $Id: MainstreamList.java,v 1.5 2006-10-25 20:43:28 cananian Exp $
+ * @version $Id: MainstreamList.java,v 1.6 2006-11-14 16:12:39 cananian Exp $
  */
 public abstract class MainstreamList {
     // hide constructor.
