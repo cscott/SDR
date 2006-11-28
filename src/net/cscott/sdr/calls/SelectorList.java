00d282e632faec2dfe715fb228ba264cdfa79e85
C. Scott Ananian <cscott@cscott.net>
2006-11-28 06:47:22 +0000

    Add 'OR' selection combination function.

--- src/net/cscott/sdr/calls/SelectorList.java
+++ src/net/cscott/sdr/calls/SelectorList.java
@@ -33,14 +33,20 @@ public abstract class SelectorList {
         GeneralFormationMatcher.makeSelector(FormationList.RH_OCEAN_WAVE);
     public static final Selector LH_OCEAN_WAVE =
         GeneralFormationMatcher.makeSelector(FormationList.LH_OCEAN_WAVE);
+    public static final Selector OCEAN_WAVE =
+        OR(RH_OCEAN_WAVE, LH_OCEAN_WAVE);
     public static final Selector RH_BOX =
         GeneralFormationMatcher.makeSelector(FormationList.RH_BOX);
     public static final Selector LH_BOX =
         GeneralFormationMatcher.makeSelector(FormationList.LH_BOX);
+    public static final Selector BOX =
+        OR(RH_BOX, LH_BOX);
     public static final Selector RH_TWO_FACED_LINE =
         GeneralFormationMatcher.makeSelector(FormationList.RH_TWO_FACED_LINE);
     public static final Selector LH_TWO_FACED_LINE =
         GeneralFormationMatcher.makeSelector(FormationList.LH_TWO_FACED_LINE);
+    public static final Selector TWO_FACED_LINE =
+        OR(RH_TWO_FACED_LINE, LH_TWO_FACED_LINE);
     public static final Selector RH_DIAMOND =
         GeneralFormationMatcher.makeSelector(FormationList.RH_DIAMOND);
     public static final Selector RH_FACING_DIAMOND =
@@ -76,14 +82,20 @@ public abstract class SelectorList {
         GeneralFormationMatcher.makeSelector(FormationList.PARALLEL_RH_WAVES);
     public static final Selector PARALLEL_LH_WAVES =
         GeneralFormationMatcher.makeSelector(FormationList.PARALLEL_LH_WAVES);
+    public static final Selector PARALLEL_WAVES =
+        OR(PARALLEL_RH_WAVES, PARALLEL_LH_WAVES);
     public static final Selector PARALLEL_RH_TWO_FACED_LINES =
         GeneralFormationMatcher.makeSelector(FormationList.PARALLEL_RH_TWO_FACED_LINES);
     public static final Selector PARALLEL_LH_TWO_FACED_LINES =
         GeneralFormationMatcher.makeSelector(FormationList.PARALLEL_LH_TWO_FACED_LINES);
+    public static final Selector PARALLEL_TWO_FACED_LINES =
+        OR(PARALLEL_RH_TWO_FACED_LINES, PARALLEL_LH_TWO_FACED_LINES);
     public static final Selector RH_COLUMN =
         GeneralFormationMatcher.makeSelector(FormationList.RH_COLUMN);
     public static final Selector LH_COLUMN =
         GeneralFormationMatcher.makeSelector(FormationList.LH_COLUMN);
+    public static final Selector COLUMN =
+        OR(RH_COLUMN, LH_COLUMN);
     public static final Selector ENDS_IN_INVERTED_LINES =
         GeneralFormationMatcher.makeSelector(FormationList.ENDS_IN_INVERTED_LINES);
     public static final Selector ENDS_OUT_INVERTED_LINES =
@@ -92,14 +104,20 @@ public abstract class SelectorList {
         GeneralFormationMatcher.makeSelector(FormationList.RH_QUARTER_TAG);
     public static final Selector LH_QUARTER_TAG =
         GeneralFormationMatcher.makeSelector(FormationList.LH_QUARTER_TAG);
+    public static final Selector QUARTER_TAG =
+        OR(RH_QUARTER_TAG, LH_QUARTER_TAG);
     public static final Selector RH_THREE_QUARTER_TAG =
         GeneralFormationMatcher.makeSelector(FormationList.RH_THREE_QUARTER_TAG);
     public static final Selector LH_THREE_QUARTER_TAG =
         GeneralFormationMatcher.makeSelector(FormationList.LH_THREE_QUARTER_TAG);
+    public static final Selector THREE_QUARTER_TAG =
+        OR(RH_THREE_QUARTER_TAG, LH_THREE_QUARTER_TAG);
     public static final Selector RH_QUARTER_LINE =
         GeneralFormationMatcher.makeSelector(FormationList.RH_QUARTER_LINE);
     public static final Selector LH_QUARTER_LINE =
         GeneralFormationMatcher.makeSelector(FormationList.LH_QUARTER_LINE);
+    public static final Selector QUARTER_LINE =
+        OR(RH_QUARTER_LINE, LH_QUARTER_LINE);
     public static final Selector RH_TWIN_DIAMONDS =
         GeneralFormationMatcher.makeSelector(FormationList.RH_TWIN_DIAMONDS);
     public static final Selector LH_TWIN_DIAMONDS =
@@ -120,15 +138,50 @@ public abstract class SelectorList {
         GeneralFormationMatcher.makeSelector(FormationList.RH_TIDAL_WAVE);
     public static final Selector LH_TIDAL_WAVE =
         GeneralFormationMatcher.makeSelector(FormationList.LH_TIDAL_WAVE);
+    public static final Selector TIDAL_WAVE =
+        OR(RH_TIDAL_WAVE, LH_TIDAL_WAVE);
     public static final Selector RH_TIDAL_TWO_FACED_LINE =
         GeneralFormationMatcher.makeSelector(FormationList.RH_TIDAL_TWO_FACED_LINE);
     public static final Selector LH_TIDAL_TWO_FACED_LINE =
         GeneralFormationMatcher.makeSelector(FormationList.LH_TIDAL_TWO_FACED_LINE);
+    public static final Selector TIDAL_TWO_FACED_LINE =
+        OR(RH_TIDAL_TWO_FACED_LINE, LH_TIDAL_TWO_FACED_LINE);
     public static final Selector RH_TIDAL_LINE =
         GeneralFormationMatcher.makeSelector(FormationList.RH_TIDAL_LINE);
     public static final Selector LH_TIDAL_LINE =
         GeneralFormationMatcher.makeSelector(FormationList.LH_TIDAL_LINE);
+    public static final Selector TIDAL_LINE =
+        OR(RH_TIDAL_LINE, LH_TIDAL_LINE);
 
+    // selector combinator
+    /** The {@link OR} function creates a Selector which matches any one of the
+     * given alternatives. */
+    public static Selector OR(final Selector... alternatives) {
+        return new Selector() {
+            @Override
+            public FormationMatch match(Formation f) throws NoMatchException {
+                for (Selector s : alternatives) {
+                    try {
+                        return s.match(f);
+                    } catch (NoMatchException e) {
+                        /* try next selector */
+                    }
+                }
+                // no matches in any selector
+                throw new NoMatchException("Couldn't match " + this);
+            }
+            @Override
+            public String toString() {
+                StringBuilder sb = new StringBuilder("OR(");
+                for (int i=0; i<alternatives.length; i++) {
+                    sb.append(alternatives[i].toString());
+                    if (i+1 < alternatives.length) sb.append(',');
+                }
+                sb.append(')');
+                return sb.toString();
+            }
+        };
+    }
 
     // unimplemented selectors
     private static final Selector _STUB_ = new Selector() {
