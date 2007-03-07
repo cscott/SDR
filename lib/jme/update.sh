62a49d4e8889d42577b39807bab762e9e6a0ccf1
C. Scott Ananian <cscott@cscott.net>
2007-03-07 19:57:05 +0000

    Update jME.

--- lib/jme/update.sh
+++ lib/jme/update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# first build 'webdist-nativelibs' in dist/jme
+# first build 'webdist-all webdist-nativelibs' in dist/jme
 JME_CVS=../../../Jme
 cp ${JME_CVS}/target/*.jar ${JME_CVS}/lib/*.{so,jar,dll,jnilib,dylib} \
    ${JME_CVS}/lib/mvn-lib-install .
-/bin/rm -f lwjgl_test.jar jmetest.jar jmetest-data.jar
+/bin/rm -f lwjgl_test.jar jmetest.jar jmetest-data.jar junit-4.1.jar
 mkdir -p jnlp
 cp ${JME_CVS}/jnlp/*.jar jnlp
