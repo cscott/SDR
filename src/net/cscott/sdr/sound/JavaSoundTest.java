0b8a76b4f3208c56ab16069ce538248e040d0ac2
C. Scott Ananian <cscott@cscott.net>
2009-02-03 08:14:10 +0000

    Apparently the javax.sound package has been removed.
    We weren't using it anyway.

--- src/net/cscott/sdr/sound/JavaSoundTest.java
+++ src/net/cscott/sdr/sound/JavaSoundTest.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.sound.*;
+//import javax.sound.*;
 import javax.sound.sampled.*;
 
 public class JavaSoundTest {
