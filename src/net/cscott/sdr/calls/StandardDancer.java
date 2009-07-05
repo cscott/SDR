5c4c0edb539d5080431742a43f8db4ac1001482f
C. Scott Ananian <cscott@cscott.net>
2009-07-05 15:35:48 -0400

    Fix primitive tags on StandardDancers; add test cases.

--- src/net/cscott/sdr/calls/StandardDancer.java
+++ src/net/cscott/sdr/calls/StandardDancer.java
@@ -2,7 +2,82 @@ package net.cscott.sdr.calls;
 
 import net.cscott.sdr.calls.TaggedFormation.Tag;
 
-/** An object representing one of the eight real dancers.
+/**
+ * An object representing one of the eight real dancers.
+ * @doc.test Properties of DANCER_1 (a head boy):
+ *  js> const Tag = TaggedFormation.Tag;
+ *  js> d = StandardDancer.COUPLE_1_BOY;
+ *  COUPLE 1 BOY
+ *  js> d.coupleNumber()
+ *  1
+ *  js> d.isHead()
+ *  true
+ *  js> d.isSide()
+ *  false
+ *  js> d.isBoy()
+ *  true
+ *  js> d.isGirl()
+ *  false
+ *  js> d.ordinal()
+ *  0
+ *  js> d.primitiveTag()
+ *  DANCER_1
+ *  js> d.matchesTag(d.primitiveTag())
+ *  true
+ *  js> d.matchesTag(Tag.BOY)
+ *  true
+ *  js> d.matchesTag(Tag.GIRL)
+ *  false
+ *  js> d.matchesTag(Tag.HEAD)
+ *  true
+ *  js> d.matchesTag(Tag.SIDE)
+ *  false
+ *  js> [d.matchesTag(t) for each (t in
+ *    >  [Tag.COUPLE_1, Tag.COUPLE_2, Tag.COUPLE_3, Tag.COUPLE_4])]
+ *  true,false,false,false
+ *  js> [d.matchesTag(t) for each (t in
+ *    >  [Tag.DANCER_1, Tag.DANCER_2, Tag.DANCER_3, Tag.DANCER_4,
+ *    >   Tag.DANCER_5, Tag.DANCER_6, Tag.DANCER_7, Tag.DANCER_8])]
+ *  true,false,false,false,false,false,false,false
+ *  js> d.matchesTag(Tag.ALL)
+ *  true
+ * @doc.test Properties of DANCER_8 (a side girl):
+ *  js> const Tag = TaggedFormation.Tag;
+ *  js> d = StandardDancer.COUPLE_4_GIRL;
+ *  COUPLE 4 GIRL
+ *  js> d.coupleNumber()
+ *  4
+ *  js> d.isHead()
+ *  false
+ *  js> d.isSide()
+ *  true
+ *  js> d.isBoy()
+ *  false
+ *  js> d.isGirl()
+ *  true
+ *  js> d.ordinal()
+ *  7
+ *  js> d.primitiveTag()
+ *  DANCER_8
+ *  js> d.matchesTag(d.primitiveTag())
+ *  true
+ *  js> d.matchesTag(Tag.BOY)
+ *  false
+ *  js> d.matchesTag(Tag.GIRL)
+ *  true
+ *  js> d.matchesTag(Tag.HEAD)
+ *  false
+ *  js> d.matchesTag(Tag.SIDE)
+ *  true
+ *  js> [d.matchesTag(t) for each (t in
+ *    >  [Tag.COUPLE_1, Tag.COUPLE_2, Tag.COUPLE_3, Tag.COUPLE_4])]
+ *  false,false,false,true
+ *  js> [d.matchesTag(t) for each (t in
+ *    >  [Tag.DANCER_1, Tag.DANCER_2, Tag.DANCER_3, Tag.DANCER_4,
+ *    >   Tag.DANCER_5, Tag.DANCER_6, Tag.DANCER_7, Tag.DANCER_8])]
+ *  false,false,false,false,false,false,false,true
+ *  js> d.matchesTag(Tag.ALL)
+ *  true
  */
 public enum StandardDancer implements Dancer {
     COUPLE_1_BOY, COUPLE_1_GIRL, COUPLE_2_BOY, COUPLE_2_GIRL,
@@ -24,14 +99,14 @@ public enum StandardDancer implements Dancer {
         case COUPLE_2: return coupleNumber()==2;
         case COUPLE_3: return coupleNumber()==3;
         case COUPLE_4: return coupleNumber()==4;
-        case DANCER_1: return ordinal()==1;
-        case DANCER_2: return ordinal()==2;
-        case DANCER_3: return ordinal()==3;
-        case DANCER_4: return ordinal()==4;
-        case DANCER_5: return ordinal()==5;
-        case DANCER_6: return ordinal()==6;
-        case DANCER_7: return ordinal()==7;
-        case DANCER_8: return ordinal()==8;
+        case DANCER_1: return ordinal()==0;
+        case DANCER_2: return ordinal()==1;
+        case DANCER_3: return ordinal()==2;
+        case DANCER_4: return ordinal()==3;
+        case DANCER_5: return ordinal()==4;
+        case DANCER_6: return ordinal()==5;
+        case DANCER_7: return ordinal()==6;
+        case DANCER_8: return ordinal()==7;
         case ALL: return true; // all matches everyone.
         default:
             assert !tag.isPrimitive();
