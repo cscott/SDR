63cccfad4dc8cf08e6b5fc0bcb694783784b7060
C. Scott Ananian <cscott@cscott.net>
2009-10-20 13:12:06 -0400

    Feasibility test for using Cassowary to implement Breather.trimOverlaps()

--- src/EDU/Washington/grad/gjb/cassowary/BreatheTest.java
+++ src/EDU/Washington/grad/gjb/cassowary/BreatheTest.java
@@ -1,12 +1,24 @@
 package EDU.Washington.grad.gjb.cassowary;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import net.cscott.sdr.util.Box;
 import net.cscott.sdr.util.Fraction;
+import net.cscott.sdr.util.Point;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-/** Simple feasibility test for SDR application. */
+/** Simple feasibility tests for SDR application. */
 public class BreatheTest {
+    /**
+     * Test formation expansion.
+     * @doc.test
+     *  js> BreatheTest.testDiamondBreathing()
+     */
     @Test
     public void testDiamondBreathing() throws ExCLError {
         ClSimplexSolver solver = new ClSimplexSolver();
@@ -48,6 +60,7 @@ public class BreatheTest {
         solver.addConstraint(new ClLinearEquation(CL.Minus(d, c), CL.Minus(f, e),
                 ClStrength.strong));
         // okay, look at solution
+        /*
         System.out.println("a="+a.value().toProperString());
         System.out.println("b="+b.value().toProperString());
         System.out.println("c="+c.value().toProperString());
@@ -55,6 +68,7 @@ public class BreatheTest {
         System.out.println("e="+e.value().toProperString());
         System.out.println("f="+f.value().toProperString());
         System.out.println("g="+g.value().toProperString());
+        */
         // automate the check
         Assert.assertEquals("a", Fraction.ZERO, a.value());
         Assert.assertEquals("b", Fraction.valueOf(2, 3), b.value());
@@ -63,5 +77,160 @@ public class BreatheTest {
         Assert.assertEquals("e", Fraction.valueOf(8, 3), e.value());
         Assert.assertEquals("f", Fraction.valueOf(10, 3), f.value());
         Assert.assertEquals("g", Fraction.valueOf(4), g.value());
+    }
+
+    /** Feasibility test.
+     * We're simulating breathing from this formation:
+     * <pre>
+     * net.cscott.sdr.calls.Formation[
+     *   location={COUPLE 3 GIRL=-2 1/2,1,w, COUPLE 2 GIRL=1 1/2,1,w, COUPLE 3 BOY=-3,0,n,[ROLL_RIGHT], COUPLE 4 BOY=-1,0,s,[ROLL_RIGHT], COUPLE 2 BOY=1,0,n,[ROLL_RIGHT], COUPLE 1 BOY=3,0,s,[ROLL_RIGHT], COUPLE 4 GIRL=-1 1/2,-1,e, COUPLE 1 GIRL=2 1/2,-1,e}
+     *   selected=[COUPLE 3 GIRL, COUPLE 2 GIRL, COUPLE 3 BOY, COUPLE 4 BOY, COUPLE 2 BOY, COUPLE 1 BOY, COUPLE 4 GIRL, COUPLE 1 GIRL]
+     * ]
+     * </pre>
+     * But for simplicity, only doing half the formation.
+     * @doc.test
+     *  js> BreatheTest.testDiamondTrimming()
+     */
+    @Test
+    public void testDiamondTrimming() throws ExCLError {
+        /* four dancers:
+         * A: [-2,-1, 0,1] n  (center -1,0)
+         * B: [0,-1, 2,1]  s  (center  1,0)
+         * C: [-1/2,-2, 1 1/2,0] e (center 1/2, -1)
+         * D: [-1 1/2,0, 1/2, 2] w (center -1/2, 1)
+         *
+         * Four variables each: l,b,t,r (left/bottom/top/right)
+         */
+        ClSimplexSolver solver = new ClSimplexSolver();
+
+        Box ab = boxFromStrings(  "-2", "-1", "0", "1" );
+        Box bb = boxFromStrings(   "0", "-1", "2", "1" );
+        Box cb = boxFromStrings("-1/2", "-2", "1 1/2", "0" );
+        Box db = boxFromStrings("-1 1/2", "0", "1/2", "2" );
+        // variables, stays, and basic required constraints
+        // (left<=right, bottom<=top; regions only shrink)
+        ClVariable[] av = makeVars(solver, "a", ab);
+        ClVariable[] bv = makeVars(solver, "b", bb);
+        ClVariable[] cv = makeVars(solver, "c", cb);
+        ClVariable[] dv = makeVars(solver, "d", db);
+        // for each pair of overlapping boxes, add constraints to prevent the
+        // overlap.
+        List<ClVariable> switches = new ArrayList<ClVariable>();
+        ClVariable sw;
+        // (1) a<->b (no overlap)
+        // (2) a<->c (0=ar<cl, 1=ct<ab)
+        sw = makePseudoBoolean(solver);
+        solver.addConstraint(new ClLinearInequality
+                (av[RIGHT], CL.Op.LEQ,
+                 CL.Plus(cv[LEFT], CL.Times(sw, cb.width()))));
+        solver.addConstraint(new ClLinearInequality
+                (CL.Plus(cv[TOP], CL.Times(sw, cb.height())), CL.Op.LEQ,
+                 CL.Plus(av[BOTTOM], cb.height())));
+        switches.add(sw);
+        // (3) a<->d (0=ar<dl, 1=at<db)
+        sw = makePseudoBoolean(solver);
+        solver.addConstraint(new ClLinearInequality
+                (av[RIGHT], CL.Op.LEQ,
+                 CL.Plus(dv[LEFT], CL.Times(sw, db.width()))));
+        solver.addConstraint(new ClLinearInequality
+                (CL.Plus(av[TOP], CL.Times(sw, db.height())), CL.Op.LEQ,
+                 CL.Plus(dv[BOTTOM], db.height())));
+        switches.add(sw);
+        // (4) b<->c (0=cr<bl, 1=ct<bb)
+        sw = makePseudoBoolean(solver);
+        solver.addConstraint(new ClLinearInequality
+                (cv[RIGHT], CL.Op.LEQ,
+                 CL.Plus(bv[LEFT], CL.Times(sw, bb.width()))));
+        solver.addConstraint(new ClLinearInequality
+                (CL.Plus(cv[TOP], CL.Times(sw, bb.height())), CL.Op.LEQ,
+                 CL.Plus(bv[BOTTOM], bb.height())));
+        switches.add(sw);
+        // (5) b<->d (0=dr<bl, 1=bt<db)
+        sw = makePseudoBoolean(solver);
+        solver.addConstraint(new ClLinearInequality
+                (dv[RIGHT], CL.Op.LEQ,
+                 CL.Plus(bv[LEFT], CL.Times(sw, bb.width()))));
+        solver.addConstraint(new ClLinearInequality
+                (CL.Plus(bv[TOP], CL.Times(sw, db.height())), CL.Op.LEQ,
+                 CL.Plus(dv[BOTTOM], db.height())));
+        switches.add(sw);
+        // (6) c<->d (no overlap)
+
+        // add handhold constraints
+        solver.addConstraint(new ClLinearEquation
+                (new ClLinearExpression(av[RIGHT]),
+                 new ClLinearExpression(bv[LEFT]),
+                 ClStrength.medium));
+
+        // this is a bad approximation at solving the mixed linear
+        // programming problem: force boolean vars to 0 or 1, depending on
+        // which they're closer to.  We should use branch-and-bound instead.
+        Collections.sort(switches, new Comparator<ClVariable>() {
+            public int compare(ClVariable v1, ClVariable v2) {
+                return v1.value().compareTo(v2.value());
+            }});
+        System.out.println(switches);
+        for (ClVariable v : switches) {
+            Fraction forced =(v.value().compareTo(Fraction.ONE_HALF) < 0) ?
+                    Fraction.ZERO : Fraction.ONE;
+            // add required constraint
+            System.out.println("Forcing "+v+" to "+forced);
+            solver.addConstraint(new ClLinearEquation(v, forced));
+        }
+
+        // output/check solution!
+        ab = boxFromVars(av);
+        bb = boxFromVars(bv);
+        cb = boxFromVars(cv);
+        db = boxFromVars(dv);
+        System.out.println("Choice vars: "+switches);
+        System.out.println("a: "+ab);
+        System.out.println("b: "+bb);
+        System.out.println("c: "+cb);
+        System.out.println("d: "+db);
+    }
+    private static int LEFT=0, BOTTOM=1, RIGHT=2, TOP=3;
+    private static Box boxFromStrings(String... points) {
+        Fraction[] v = new Fraction[points.length];
+        for (int i=0; i<points.length; i++)
+            v[i] = Fraction.valueOf(points[i]);
+        return new Box(new Point(v[LEFT], v[BOTTOM]), new Point(v[RIGHT], v[TOP]));
+    }
+    private static ClVariable[] makeVars(ClSimplexSolver s, String prefix, Box b) throws ExCLError {
+        ClVariable[] v = new ClVariable[4];
+        v[LEFT] = new ClVariable(prefix+"l", b.ll.x);
+        v[BOTTOM] = new ClVariable(prefix+"b", b.ll.y);
+        v[RIGHT] = new ClVariable(prefix+"r", b.ur.x);
+        v[TOP] = new ClVariable(prefix+"t", b.ur.y);
+        for (int i=0; i<4; i++)
+            s.addStay(v[i]);
+        // required constraints: l<r, b<t
+        s.addConstraint(new ClLinearInequality(v[LEFT], CL.Op.LEQ, v[RIGHT]));
+        s.addConstraint(new ClLinearInequality(v[BOTTOM], CL.Op.LEQ, v[TOP]));
+        // ONLY ALLOW SHRINKING REGIONS to resolve overlaps
+        // this ensures that overlaps in the solution are a subset of
+        // those currently present, which keeps the problem linear
+        s.addConstraint(new ClLinearInequality(v[LEFT], CL.Op.GEQ, b.ll.x));
+        s.addConstraint(new ClLinearInequality(v[BOTTOM], CL.Op.GEQ, b.ll.y));
+        s.addConstraint(new ClLinearInequality(v[RIGHT], CL.Op.LEQ, b.ur.x));
+        s.addConstraint(new ClLinearInequality(v[TOP], CL.Op.LEQ, b.ur.y));
+        return v;
+    }
+    private static ClVariable makePseudoBoolean(ClSimplexSolver s) throws ExCLError {
+        ClVariable v = new ClVariable();
+        // strong constraint that this is either 0 or 1
+        s.addConstraint(new ClLinearEquation(v, Fraction.ZERO, ClStrength.strong));
+        s.addConstraint(new ClLinearEquation(v, Fraction.ONE, ClStrength.strong));
+        return v;
+    }
+    private static Box boxFromVars(ClVariable[] vars) {
+        return new Box(new Point(vars[LEFT].value(), vars[BOTTOM].value()),
+                       new Point(vars[RIGHT].value(), vars[TOP].value()));
+    }
+
+    public static void main(String[] args) throws ExCLError {
+        BreatheTest bt = new BreatheTest();
+        bt.testDiamondBreathing();
+        bt.testDiamondTrimming();
     }
 }
