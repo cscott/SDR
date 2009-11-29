da3a917c7a8ebe3bb36074728e245ed4347cadb5
C. Scott Ananian <cscott@cscott.net>
2009-11-29 16:39:00 -0500

    Reflect latest versions of call definitions in run-*.sh helper scripts.
    By adding the 'resources' directory to the start of the classpath, we avoid
    the need to rebuild the jar file just to reflect a tweak in the call definition
    lists.

--- run-pmsd.sh
+++ run-pmsd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec java -cp sdr.jar:lib/dev/jline-0.9.94.jar:lib/dev/jdoctest/rhino-1.7r2-rc3.jar:lib/antlr-runtime-3.2.jar -ea:net.cscott... net.cscott.sdr.PMSD "$@"
+exec java -cp resources:sdr.jar:lib/dev/jline-0.9.94.jar:lib/dev/jdoctest/rhino-1.7r2-rc3.jar:lib/antlr-runtime-3.2.jar -ea:net.cscott... net.cscott.sdr.PMSD "$@"
