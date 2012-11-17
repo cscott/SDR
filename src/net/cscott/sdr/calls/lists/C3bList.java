af92c34c852749f324a75343c3ce43db4b80e56d
C. Scott Ananian <cscott@cscott.net>
2012-11-17 15:59:29 -0500

    Add concept definitions for 'twosome'.
    Doesn't work yet, but at least it parses.

--- src/net/cscott/sdr/calls/lists/C3bList.java
+++ src/net/cscott/sdr/calls/lists/C3bList.java
@@ -6,10 +6,15 @@ import java.util.List;
 import net.cscott.sdr.calls.Call;
 import net.cscott.sdr.calls.DanceState;
 import net.cscott.sdr.calls.Evaluator;
+import net.cscott.sdr.calls.FormationList;
 import net.cscott.sdr.calls.Program;
+import net.cscott.sdr.calls.MatcherList;
 import net.cscott.sdr.calls.ast.Expr;
 import net.cscott.sdr.calls.grm.Grm;
 import net.cscott.sdr.calls.grm.Rule;
+import net.cscott.sdr.calls.lists.A1List.SolidEvaluator;
+import net.cscott.sdr.calls.lists.A1List.SolidMatch;
+import net.cscott.sdr.calls.lists.A1List.SolidType;
 import net.cscott.sdr.calls.lists.BasicList.LRMEvaluator;
 import net.cscott.sdr.calls.lists.BasicList.LRMType;
 import net.cscott.sdr.util.Fraction;
@@ -57,6 +62,54 @@ public abstract class C3bList {
         public Evaluator getEvaluator(DanceState ds, List<Expr> args) {
             assert args.size()==1;
             return new LRMEvaluator(LRMType.MIRROR, args.get(0));
+        }
+    };
+
+    public static final Call TANDEM_TWOSOME = new C3BCall("tandem twosome") {
+        @Override
+        public int getMinNumberOfArguments() { return 1; }
+        @Override
+        public Rule getRule() {
+            Grm g = Grm.parse("tandem twosome <0=anything>");
+            return new Rule("anything", g, Fraction.valueOf(-15));
+        }
+        @Override
+        public Evaluator getEvaluator(DanceState ds, List<Expr> args) {
+            assert args.size() == 1;
+            return new SolidEvaluator(args.get(0), FormationList.TANDEM,
+                                      SolidMatch.ALL, SolidType.TWOSOME);
+        }
+    };
+
+    public static final Call COUPLES_TWOSOME = new C3BCall("couples twosome") {
+        @Override
+        public int getMinNumberOfArguments() { return 1; }
+        @Override
+        public Rule getRule() {
+            Grm g = Grm.parse("couples twosome <0=anything>");
+            return new Rule("anything", g, Fraction.valueOf(-15));
+        }
+        @Override
+        public Evaluator getEvaluator(DanceState ds, List<Expr> args) {
+            assert args.size() == 1;
+            return new SolidEvaluator(args.get(0), FormationList.COUPLE,
+                                      SolidMatch.ALL, SolidType.TWOSOME);
+        }
+    };
+
+    public static final Call SIAMESE_TWOSOME = new C3BCall("siamese twosome") {
+        @Override
+        public int getMinNumberOfArguments() { return 1; }
+        @Override
+        public Rule getRule() {
+            Grm g = Grm.parse("siamese twosome <0=anything>");
+            return new Rule("anything", g, Fraction.valueOf(-15));
+        }
+        @Override
+        public Evaluator getEvaluator(DanceState ds, List<Expr> args) {
+            assert args.size() == 1;
+            return new SolidEvaluator(args.get(0), "siamese",
+                                      MatcherList.SIAMESE, SolidType.TWOSOME);
         }
     };
 }
