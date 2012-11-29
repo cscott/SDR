a865b84ac604577cc9d5658825596c3ff81eee37
C. Scott Ananian <cscott@cscott.net>
2012-11-29 03:35:13 -0500

    Transform generated ANTLR sources to eliminate "class too large" errors.
    Our C4 grammar is getting huge!

--- src/net/cscott/sdr/calls/grm/AntlrPostProcessor.java
+++ src/net/cscott/sdr/calls/grm/AntlrPostProcessor.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -24,8 +25,8 @@ public class AntlrPostProcessor {
         File f = new File(args[0].replaceFirst("\\.g$", "Parser.java"));
         Reader r=new InputStreamReader(new FileInputStream(f), "utf-8");
         String contents = readAll(r);
-        // ok, look for the big initializers
-        String nContents = p.matcher(contents).replaceAll(replacement);
+        // ok, look for the big initializers and break them up
+        String nContents = replaceBigInitializers(contents);
         if (!contents.equals(nContents)) {
             System.err.println("Rewriting: "+f.getName());
             Writer fw = new OutputStreamWriter(new FileOutputStream(f), "utf-8");
@@ -33,14 +34,58 @@ public class AntlrPostProcessor {
             fw.close();
         }
     }
+    /* this size limit is an underestimate, since each piece is string-escaped
+     * and each element contains leading whitespace.  still, this is a
+     * reasonable limit. */
+    private static final int STRING_SIZE_LIMIT = 65536;
+    private static String replaceBigInitializers(String contents) {
+        Matcher m = p.matcher(contents);
+        StringBuffer sb = new StringBuffer();
+        while (m.find()) {
+            m.appendReplacement(sb, "");
+            String ind = m.group(1);
+            String fld = m.group(2);
+            String value = m.group(3);
+            /* now split the string array into smaller pieces */
+            String[] pieces = elemSep.split(value);
+            StringBuilder buf = new StringBuilder();
+            int n = 0;
+            for (int i=0; i<pieces.length; i++) {
+                buf.append(pieces[i]);
+                if (buf.length() > STRING_SIZE_LIMIT || i==(pieces.length-1)) {
+                    /* emit piece */
+                    sb.append(ind+"private static class "+fld+"Class"+n+" {\n");
+                    sb.append(ind+"    static final String[] value = {");
+                    sb.append(buf);
+                    sb.append("};\n");
+                    sb.append(ind+"}\n");
+                    buf.setLength(0);
+                    n++;
+                } else {
+                    buf.append(",");
+                }
+            }
+            /* now put all the pieces together */
+            sb.append(ind+"static final String[] "+fld+";\n");
+            sb.append(ind+"static {\n");
+            sb.append(ind+"    java.util.List<String> tmp = ");
+            sb.append(               "new java.util.ArrayList<String>();\n");
+            for (int i=0; i<n; i++) {
+                sb.append(ind+"    tmp.addAll(java.util.Arrays.asList("+
+                                     fld+"Class"+i+".value));\n");
+            }
+            sb.append(ind+"    "+fld+" = ");
+            sb.append(               "tmp.toArray(new String[tmp.size()]);\n");
+            sb.append(ind+"}");
+        }
+        m.appendTail(sb);
+        return sb.toString();
+    }
     private static Pattern p = Pattern.compile
         ("^(\\s*?)static final String\\[\\] (DFA[0-9]+_transitionS) = \\{(.*?)\\};$",
          Pattern.MULTILINE | Pattern.DOTALL);
-    private static String replacement =
-        "$1private static class $2Class {\n"+
-        "$1    static final String[] value = {$3};\n" +
-        "$1}\n"+
-        "$1static final String[] $2 = $2Class.value;";
+    private static Pattern elemSep = Pattern.compile
+        ("(?<=\"),$", Pattern.MULTILINE);
 
     private static String readAll(Reader r) throws IOException {
         StringBuilder sb = new StringBuilder();
