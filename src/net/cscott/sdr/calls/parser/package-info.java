986a4852cc704cb28eacd5e271bc88eceb5d3627
C. Scott Ananian <cscott@cscott.net>
2012-12-07 10:28:37 -0500

    Build the Call Definition Language Manual and link to it from the javadoc.

--- src/net/cscott/sdr/calls/parser/package-info.java
+++ src/net/cscott/sdr/calls/parser/package-info.java
@@ -1,5 +1,8 @@
 /**
  * <a href="http://www.antlr.org/">ANTLRv3</a> grammars for parsing call
  * definition files and AST representations.
+ * <p>
+ * See the <a href="doc-files/calldefs.pdf">Call Definition Language Manual</a>
+ * for a description of the language processed by this package.
  */
 package net.cscott.sdr.calls.parser;
