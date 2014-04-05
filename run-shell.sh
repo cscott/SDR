ad53e5698b026751e3c54e7a178900f7306ccd92
C. Scott Ananian <cscott@cscott.net>
2014-04-05 15:04:12 -0400

    Update Rhino JavaScript engine to 1.7R5pre.
    Include doctest line-splitting fix from
    https://github.com/cscott/rhino/tree/doctest-nl

--- run-shell.sh
+++ run-shell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec java -cp resources:sdr.jar:lib/dev/jline-0.9.94.jar:lib/dev/jdoctest/rhino-1.7r2-rc3.jar:lib/antlr-runtime-3.4.jar -ea:net.cscott... org.mozilla.javascript.tools.shell.Main -version 170 "$@"
+exec java -cp resources:sdr.jar:lib/dev/jline-0.9.94.jar:lib/dev/jdoctest/rhino-1.7r5pre.jar:lib/antlr-runtime-3.4.jar -ea:net.cscott... org.mozilla.javascript.tools.shell.Main -version 170 "$@"
