25e33f605de6757ab41b9fa290ab5df47abc4982
C. Scott Ananian <cscott@cscott.net>
2014-04-16 05:36:11 -0400

    Return non-zero exit code if run-pmsd.sh fails.

--- run-pmsd.sh
+++ run-pmsd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp resources:sdr.jar:lib/dev/jline-0.9.94.jar:lib/dev/jdoctest/rhino-1.7r5pre.jar:lib/antlr-runtime-3.4.jar -ea:net.cscott... net.cscott.sdr.PMSD "$@" || ( echo "== FAILED ==" 1>&2 )
+java -cp resources:sdr.jar:lib/dev/jline-0.9.94.jar:lib/dev/jdoctest/rhino-1.7r5pre.jar:lib/antlr-runtime-3.4.jar -ea:net.cscott... net.cscott.sdr.PMSD "$@" || ( echo "== FAILED ==" 1>&2 ; exit 1 )
