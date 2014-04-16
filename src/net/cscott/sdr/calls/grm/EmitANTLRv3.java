727b73c328be8b34924339ea15919f4416c40e9b
C. Scott Ananian <cscott@cscott.net>
2014-04-16 01:39:01 -0400

    Break up large grammar productions to avoid "method too large" errors.
    Antlr creates a single method for each lhs in a grammar production.
    As the C4 grammar continues to grow, we are running into limits on
    the permissible size of a Java method (and try-statement region size).
    
    Split productions as necessary to work around this issue.

--- src/net/cscott/sdr/calls/grm/EmitANTLRv3.java
+++ src/net/cscott/sdr/calls/grm/EmitANTLRv3.java
@@ -1,6 +1,8 @@
 package net.cscott.sdr.calls.grm;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import net.cscott.jutil.MultiMap;
 import net.cscott.sdr.calls.grm.Grm.Mult;
@@ -9,6 +11,9 @@ import net.cscott.sdr.calls.grm.Grm.Terminal;
 
 /** Emit an ANTLRv3 "natural language" grammar for calls. */
 public class EmitANTLRv3 extends AbstractEmit {
+    // productions above this size will be broken up
+    private static int MAXIMUM_ALTERNATES = 250;
+
     private NumberParams np=null;
     private EmitANTLRv3() { }
     public static String emit(String parserName,
@@ -16,14 +21,17 @@ public class EmitANTLRv3 extends AbstractEmit {
         EmitANTLRv3 ea = new EmitANTLRv3();
         // collect all the rules with the same LHS
         MultiMap<String,RuleAndAction> mm = collectLHS(l);
+        // break up any rules with too many alternates in order to
+        // avoid "method too large" errors from the java compiler
+        splitLargeProductions(mm);
         // now emit the rules.
         String NL = System.getProperty("line.separator");
         StringBuilder sb = new StringBuilder();
         for (String lhs : sorted(mm.keySet())) {
-	    if (lhs.endsWith("_suffix"))
-		sb.append("fragment "+lhs+"[Expr a]");
-	    else
-		sb.append(lhs);
+            if (lhs.endsWith("_suffix"))
+                sb.append("fragment "+lhs+"[Expr a]");
+            else
+                sb.append(lhs);
             sb.append(" returns [Expr r]");
             sb.append(NL);
             boolean first = true;
@@ -52,6 +60,45 @@ public class EmitANTLRv3 extends AbstractEmit {
         // done.
         return result;
     }
+    private static void splitLargeProductions(final MultiMap<String,RuleAndAction> rules) {
+        List<String> toSplit = new ArrayList<String>();
+        for (String lhs : rules.keySet())
+            if (rules.getValues(lhs).size() > MAXIMUM_ALTERNATES)
+                toSplit.add(lhs);
+        for (String lhs: toSplit) {
+            List<RuleAndAction> rhs =
+                new ArrayList<RuleAndAction>(rules.getValues(lhs));
+            // split list in half
+            List<RuleAndAction> rhs1 = rhs.subList(0, rhs.size()/2);
+            List<RuleAndAction> rhs2 = rhs.subList(rhs1.size(), rhs.size());
+            // name new part
+            String nlhs = uniqueLHS(lhs, rules.keySet());
+            // tie new lhs to old lhs
+            rules.remove(lhs);
+            rules.addAll(lhs, rhs1);
+            rules.add(lhs, new RuleAndAction
+                      (new Rule(lhs, new Nonterminal(nlhs, null, 0), null),
+                       "r=a;"));
+            // rewrite rhs2 to use new lhs
+            for (RuleAndAction ra : rhs2) {
+                Rule r = ra.rule;
+                rules.add(nlhs, new RuleAndAction
+                          (new Rule(nlhs, r.rhs, r.prec, r.options),
+                           ra.action));
+            }
+        }
+        // recurse to split again in case we're still not small enough.
+        if (!toSplit.isEmpty())
+            splitLargeProductions(rules);
+    }
+    private static String uniqueLHS(String s, Set<String> lhs) {
+        s = s.replaceFirst("(_split)+$", "");
+        for(int i = 1; ; i++) {
+            String candidate = s + "_split" + i;
+            if (!lhs.contains(candidate))
+                return candidate;
+        }
+    }
     private static Grm[] matchSuffix(Grm rhs) {
         // oh, for a pattern-matching language, oh
         // Grm.Concat(....., Grm.Mult(Grm.NonTerminal(..."_suffix"), STAR))
@@ -72,7 +119,7 @@ public class EmitANTLRv3 extends AbstractEmit {
         return new Grm[] { head, nt };
     }
     private static Grm[] nomatch(Grm g) { return new Grm[] { g, null }; }
-    
+
     @Override
     public String visit(Terminal t) {
         // quote literals.
