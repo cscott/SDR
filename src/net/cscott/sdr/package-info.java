d3137c41044cf9b2f33b866b47a73fdee24583b4
C. Scott Ananian <cscott@cscott.net>
2014-04-25 11:02:20 -0400

    Define: relocate, zing, lines <anything> thru.
    Note that there is a grammar conflict with 'lines <anything> thru' due to
    ANTLR not being LL(k) but instead SLL(k). Hacked around it for the moment.
    
    Also tweaked the definition of 'exchange the diamond(s)' to allow the
    optional plural.

--- src/net/cscott/sdr/package-info.java
+++ src/net/cscott/sdr/package-info.java
@@ -21,6 +21,7 @@
  *   parse-2 at line 7
  *   pass-to-the-center-2 at line 8
  *   pass-to-the-center-3 at line 26
+ *   relocate-singer at line 38
  *   right-roll-to-a-wave at line 46
  *   square-chain-the-top at line 15
  *   square-thru at line 22
